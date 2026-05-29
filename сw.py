5a4f05c1611d7bf58f1e6a998da9dcfa929ff912
Alice <alisapetroffa24@gmail.com>
2026-05-29 17:29:28 +0300

    дзз останье

--- сw.py
+++ сw.py
@@ -4,32 +4,35 @@ class BankAccount:
 
     def add_money(self, amount):
         if amount < 0:
-            raise ValueError("Сума поповнення не може бути від'ємною")
+            raise ValueError("Сума поповнення коштів не може бути від'ємною")
         self.money += amount
         print(f"Поповнено на: {amount}. Баланс: {self.money}")
 
     def withdraw(self, amount):
         if amount == 0:
-            raise ValueError("Сума зняття не може бути 0")
-
+            raise ValueError("Сума не може бути 0")
         if amount < 0:
             raise ValueError("Не можна знімати мінус")
         if amount > self.money:
             diff = amount - self.money
-            print(f"Недостатньо грошівв. Потрібно ще {diff} грн.")
-            answer = input(f"Взяти кредит {diff} грн? (так/ні): ")
+            print(f"У вас недостаток грошей. Потрібно ще {diff} грн.")
+            answer = input(f"Взяти кредит {diff} грн?: ")
+
             if answer.lower() == "так":
-                self.money += diff
-                print(f"Кредит надан:). Баланс поповнено на {diff} грн.")
-                # Здесь можно добавить логику хранения даты для расчета процентов
+                days = int(input("На скільки днів берете кредит? "))
+                percent = 0.03 if days <= 30 else 0.04
+                total_to_pay = diff + (diff * percent)
+                self.money = 0
+                print(f"Кредит надано: {diff} грн.")
+                print(f"Сума до повернення через {days} днів: {total_to_pay:.2f} грн.")
             else:
                 raise ValueError("Операцію скасовано через відсутність коштів")
-
-        self.money -= amount
-        print(f"Знято: {amount}. Залишок: {self.money}")
-account = BankAccount(100)
+        else:
+            self.money -= amount
+            print(f"Знято: {amount}. Залишок: {self.money}")
 try:
-    take = int(input("Скільки зняти коштів? "))
+    account = BankAccount(100)
+    take = int(input("Скільки зняти грошів? "))
     account.withdraw(take)
 except ValueError as e:
     print("Помилка:", e)
